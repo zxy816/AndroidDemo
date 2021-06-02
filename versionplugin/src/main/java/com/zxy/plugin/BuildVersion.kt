000958084af502bbef51a5b3c038ad6e5e37accd
softOfAndroid <zxy1988816@163.com>
2021-06-02 17:28:32 +0800

    .

---
+++ versionplugin/src/main/java/com/zxy/plugin/BuildVersion.kt
@@ -0,0 +1,9 @@
+package com.zxy.plugin
+
+object BuildVersion {
+    const val compileSdkVersion = 29
+    const val targetSdkVersion = 26
+    const val minSdkVersion = 22
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
---
+++ versionplugin/src/main/java/com/zxy/plugin/VersionPlugin.kt
@@ -0,0 +1,14 @@
+package com.zxy.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class VersionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        
+    }
+
+    companion object{
+
+    }
+}
